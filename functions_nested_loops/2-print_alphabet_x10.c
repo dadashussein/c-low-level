808347a2b5e4a564e5bd2ad2e8ef6a4cb35c864b
Dadash Huseynzade <7416@holbertonstudents.com>
2023-10-17 00:42:57 -0700

    c prototype

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
