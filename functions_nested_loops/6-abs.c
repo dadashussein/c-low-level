5e526ff08618b95ca3a16a8f88bbe68b494cbfbe
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-10-17 02:25:32 -0700

    abs, isAlpha, isLower

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The number to be checked
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
