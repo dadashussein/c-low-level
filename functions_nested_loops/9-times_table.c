fb262c57b9b04ac3e498d7ceda23e4963019b44a
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-10-17 12:46:15 -0700

    loop, if

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			if ((i * j) <= 9)
+				_putchar(' ');
+			else
+				_putchar((i * j) / 10 + '0');
+		_putchar((i * j) % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
