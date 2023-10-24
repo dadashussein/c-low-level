d99c97d1fa064fb242b2ded6b88935b043c3c201
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-10-23 23:38:57 -0700

    fix string rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		;
 	for (i = i - 1; i >= 0; i--)
-		_putchar(str[i]);
+		_putchar([i]);
 
 	_putchar('\n');
 }
