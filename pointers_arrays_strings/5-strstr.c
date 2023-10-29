fb31ec67315382228fcc5775e033b7610b5c099d
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-10-29 07:58:48 -0700

    fix function

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,42 @@
+#include <stdlib.h>
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: pointer to string to search through
- * @accept: array of bytes to search for
- * Return: pointer to the byte in s that matches one of the bytes in accept
- * or return NULL if no bytes are found.
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
  */
-
-char *_strpbrk(char *s, char *accept)
+int _strncmp(char *s1, char *s2, unsigned int b)
 {
 	unsigned int i;
-
-	while (*s != '\0')
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
-		{
-			if (*s == accept[i])
-				return (s);
-		}
-		s++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (i - b);
+}
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int len;
+	/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	/*compare substring*/
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
 }
