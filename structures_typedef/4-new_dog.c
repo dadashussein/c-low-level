d48118e9df4d6fd9a76956e1896756564fbe3a7a
Dadash <huseynzade.dadas@gmail.com>
2023-11-20 23:08:35 +0400

    update functions

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,20 +14,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(dog_t));
-	if (my_dog != NULL)
+	if (my_dog == NULL)
+		return (NULL);
+
+	my_dog->name = strdup(name);
+	if (!my_dog->name)
 	{
-		my_dog->name = strdup(name);
-		my_dog->owner = strdup(owner);
+		free(my_dog);
+		return (NULL);
+	}
 
-		if (my_dog->name == NULL || my_dog->owner == NULL)
-		{
-			free(my_dog->name);
-			free(my_dog->owner);
-			free(my_dog);
-			return (NULL);
-		}
-
-		my_dog->age = age;
+	my_dog->age = age;
+	my_dog->owner = strdup(owner);
+	if (!my_dog->owner)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
 	}
 
 	return (my_dog);
