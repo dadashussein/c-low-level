ef8fcfc4505c6effa79ecb63c5e1b883c704d6d0
Dadash <huseynzade.dadas@gmail.com>
2023-11-22 18:30:15 +0400

    pointers are done

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function
+ * @array: given array
+ * @size: size of the array
+ * @action: a pointer to the function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+
+}
