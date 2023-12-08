874b83ca58258f860a169ed52ff65847d5ea49af
Dadash <huseynzade.dadas@gmail.com>
2023-12-08 20:44:10 +0400

    update add node function

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,13 +9,18 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = *head;
+	temp = malloc(sizeof(dlistint_t));
 
 	if (temp == NULL)
 		return (NULL);
 
 	temp->n = n;
+	temp->prev = NULL;
 	temp->next = *head;
 	*head = temp;
+	if (*head != NULL)
+		(*head)->prev = temp;
+
 	return (temp);
 }
