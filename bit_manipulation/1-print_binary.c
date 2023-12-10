33be2169ec6322ecbbe4d665c26d62e223dd6cb9
Dadash <huseynzade.dadas@gmail.com>
2023-12-10 11:22:11 +0400

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints decimal
+ * @n: long integer
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n % 2) + '0');
+}
