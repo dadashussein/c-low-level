0438dd060b23513108dbdb372bdf8dc1ecaea4cc
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-12-15 20:28:00 +0400

    Add create_node function and update hash_table_set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,38 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
+ * create_node - creates new node
+ * @key: key
+ * @value: value associated key
+ *
+ * Return: new node
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
  * hash_table_set - adds an element
  * @ht: hash table
  * @key: key
@@ -14,31 +46,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	hash_node_t *newElement;
+	hash_node_t *current;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-	newElement = malloc(sizeof(hash_node_t));
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	newElement = create_node(key, value);
 	if (newElement == NULL)
 		return (0);
-
-	newElement->key = strdup(key);
-	if (newElement->key == NULL)
-	{
-		free(newElement);
-		return (0);
-	}
-
-	newElement->value = strdup(value);
-	if (newElement->value == NULL)
-	{
-		free(newElement->key);
-		free(newElement);
-		return (0);
-	}
-
 	newElement->next = ht->array[index];
 	ht->array[index] = newElement;
 
